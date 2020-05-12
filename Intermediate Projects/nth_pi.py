e45f7f7e2903de5d7e0c82f43a89ff9de975901c
hjohns14 <hjohns1737@gmail.com>
2020-05-12 18:00:41 -0500

    Update nth_pi.py

--- Intermediate Projects/nth_pi.py
+++ Intermediate Projects/nth_pi.py
@@ -1,7 +1,16 @@
 import math
-pi = math.pi
 
-n = 5
+'''This project prompts the user to enter a number and prints
+    pi to with the input amount of decimal places.
+    This program is only as accurate as the input of pi. '''
+
+
+def betterPi():
+    #impliment method to calculate higher precision pi
+    pass
+
+pi = math.pi
+n = str(input("Enter your desired number of decimal places: "))
 f ="." + str(n) + "f"
 
 print(format(math.pi, f))
